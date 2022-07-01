214f41818e48e23da47268fc781f98e6e71a7f25
HtinSharKyaw <htinsharrkyaw@gmail.com>
2022-06-30 21:59:03 -0500

    second commit

--- lab6-security-assignment/src/main/java/com/example/lab6securityassignment/aop/annotations/OffensiveWarn.java
+++ lab6-security-assignment/src/main/java/com/example/lab6securityassignment/aop/annotations/OffensiveWarn.java
@@ -4,5 +4,5 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
-public @interface filterInterface {
+public @interface OffensiveWarn {
 }
--- lab6-security-assignment/src/main/java/com/example/lab6securityassignment/aop/aspects/UserOffensiveCount.java
+++ lab6-security-assignment/src/main/java/com/example/lab6securityassignment/aop/aspects/UserOffensiveCount.java
@@ -1,2 +1,32 @@
-package com.example.lab6securityassignment.aop.aspects;public class UserOffensiveCount {
+package com.example.lab6securityassignment.aop.aspects;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class UserOffensiveCount {
+    private final int count;
+
+    private final LocalDateTime lastRequest;
+
+    private int timeout = 15 * 60;
+
+    public UserOffensiveCount(int count, LocalDateTime lastRequest) {
+        this.count = count;
+        this.lastRequest = lastRequest;
+    }
+
+    public long timeOut() {
+        Duration diff = Duration.between(lastRequest, LocalDateTime.now());
+
+        if (diff.toSeconds() < timeout) {
+            return timeout - diff.toSeconds();
+        }
+
+        return 0;
+    }
 }
